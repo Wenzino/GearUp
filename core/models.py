f28aaf76a47df973e76c041d604a6927c93d9494
Wen Xuan <54104610+Wenzino@users.noreply.github.com>
2025-02-02 04:27:56 +0200

    . . .

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
+from django_countries import countries
 
 # Product
 class Product(models.Model):
@@ -168,3 +169,17 @@ class Review(models.Model):
 
     def __str__(self):
         return f'{self.user.username} - {self.product.name} - {self.rating} estrelas'
+
+class BillingAddress(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField(blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=10, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return f"Endereço de {self.user.username}"
+    
+    def get_country_display(self):
+        return dict(countries).get(self.country, '')
